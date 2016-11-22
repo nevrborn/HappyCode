7c3758ccb2b08b2f9f239843992ee7ec080358ef
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2016-11-22 13:03:08 +0100

    started with the StartPage xml, activity and fragment. Add some strings.

---
+++ app/src/main/java/com/cloud9/android/happycode/StartPageFragment.java
@@ -0,0 +1,14 @@
+package com.cloud9.android.happycode;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by paulvancappelle on 22-11-16.
+ */
+
+public class StartPageFragment extends Fragment {
+
+    public static Fragment newInstance() {
+        return new StartPageFragment();
+    }
+}
