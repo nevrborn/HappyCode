f191ddf2421e66ccabe93ab3aa21a1ba74a2f0e4
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2016-11-22 14:40:42 +0100

    changes in the Start Page

--- app/src/main/java/com/cloud9/android/happycode/StartPageActivity.java
+++ app/src/main/java/com/cloud9/android/happycode/StartPageActivity.java
@@ -1,5 +1,6 @@
 package com.cloud9.android.happycode;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -9,15 +10,15 @@ public class StartPageActivity extends SingleFragmentActivity {
 
 
     /*
-    *
+    * create Intent to start this activity
     */
-//    @Override
-//    public Intent newIntent() {
-//        return StartPageFragment.newInstance();
-//    }
+    public Intent newIntent(Context context) {
+        return new Intent(context, StartPageActivity.class);
+    }
+
 
     /*
-    *
+    * Method to create fragment from Activity
     */
     @Override
     protected Fragment createFragment() {
--- app/src/main/java/com/cloud9/android/happycode/StartPageFragment.java
+++ app/src/main/java/com/cloud9/android/happycode/StartPageFragment.java
@@ -1,6 +1,12 @@
 package com.cloud9.android.happycode;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by paulvancappelle on 22-11-16.
@@ -8,7 +14,40 @@ import android.support.v4.app.Fragment;
 
 public class StartPageFragment extends Fragment {
 
+    Button mStartButton;
+
+
+    /*
+    * create new instance
+    */
     public static Fragment newInstance() {
         return new StartPageFragment();
     }
+
+
+    @Nullable
+    @Override
+    /*
+    * onCreateView
+    */
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_start_page, container, false);
+
+        // set the references
+        mStartButton = (Button) view.findViewById(R.id.button_start);
+
+        // set listeners
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // add code here to start questions
+            }
+        });
+
+
+        return view;
+    }
+
+
+
 }
