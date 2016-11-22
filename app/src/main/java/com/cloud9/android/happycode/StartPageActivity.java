0bb71c1c279584104f841b8cc815dee4555d330a
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2016-11-22 11:14:39 +0100

    initial commit

---
+++ app/src/main/java/com/cloud9/android/happycode/StartPageActivity.java
@@ -0,0 +1,13 @@
+package com.cloud9.android.happycode;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartPageActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
