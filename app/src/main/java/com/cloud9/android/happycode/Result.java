5c2374d9b199a2e305eee1d1bc8a3824a7dfff88
Jarle <jarle.matland@gmail.com>
2016-11-23 15:51:39 +0100

    - deleted the Strengths array from Result. only need it one place

--- app/src/main/java/com/cloud9/android/happycode/Result.java
+++ app/src/main/java/com/cloud9/android/happycode/Result.java
@@ -16,30 +16,6 @@ public class Result {
     private int mNo2Strength;
     private int mNo3Strength;
 
-    // Adding all the Strenghts and descriptions to an array - mStrength
-    private Strength[] mStrengths = new Strength[]{
-            new Strength(R.string.strength_leader_title, R.string.strength_leader, R.string.strength_leader_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_storyteller_title, R.string.strength_storyteller, R.string.strength_storyteller_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_challenger_title, R.string.strength_challenger, R.string.strength_challenger_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_networker_title, R.string.strength_networker, R.string.strength_networker_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_moodmaker_title, R.string.strength_moodmaker, R.string.strength_moodmaker_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_organiser_title, R.string.strength_organiser, R.string.strength_organiser_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_guardian_title, R.string.strength_guardian, R.string.strength_guardian_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_planner_title, R.string.strength_planner, R.string.strength_planner_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_hardworker_title, R.string.strength_hardworker, R.string.strength_hardworker_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_administrator_title, R.string.strength_administrator, R.string.strength_administrator_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_motivator_title, R.string.strength_motivator, R.string.strength_motivator_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_connector_title, R.string.strength_connector, R.string.strength_connector_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_unifier_title, R.string.strength_unifier, R.string.strength_unifier_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_inspirer_title, R.string.strength_inspirer, R.string.strength_inspirer_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_feeler_title, R.string.strength_feeler, R.string.strength_feeler_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_strategist_title, R.string.strength_strategist, R.string.strength_strategist_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_visionary_title, R.string.strength_visionary, R.string.strength_visionary_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_pathfinder_title, R.string.strength_pathfinder, R.string.strength_pathfinder_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_researcher_title, R.string.strength_researcher, R.string.strength_researcher_description, R.mipmap.strength_leader),
-            new Strength(R.string.strength_thinker_title, R.string.strength_thinker, R.string.strength_thinker_description, R.mipmap.strength_leader)
-    };
-
     private static Result mResult;
 
 
@@ -60,13 +36,6 @@ public class Result {
 
     public void deleteResult() {
         mResult = null;
-    }
-
-    /*
-    * getter to get a strength object based on the id of the first strength
-    */
-    public Strength getNoXStrength(int NoXStrengthId) {
-        return mStrengths[NoXStrengthId];
     }
 
 
