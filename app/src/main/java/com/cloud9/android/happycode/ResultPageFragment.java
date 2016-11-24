cf9f0bda77ea2b1ee3672ea326f2f197d06e6f4a
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2016-11-24 12:24:15 +0100

    changes in the start page files to get hamburger menu. Does not work yet.

--- app/src/main/java/com/cloud9/android/happycode/ResultPageFragment.java
+++ app/src/main/java/com/cloud9/android/happycode/ResultPageFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +12,19 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 /**
  * Created by paulvancappelle on 23-11-16.
  */
 public class ResultPageFragment extends Fragment {
 
+    private static final String TAG = "ResultPageFragment";
+    private static final String KEY_CURRENT_STRENGTH = "currentStrength";
+
     private int mNr1Strength;
     private int mNr2Strength;
     private int mNr3Strength;
+    private int mCurrentStrength; // which result is currently selected (0, 1, ...)
     private Result mResult;
 
     private ImageView mResultIcon1;
@@ -26,7 +32,7 @@ public class ResultPageFragment extends Fragment {
     private ImageView mResultIcon3;
     private TextView mStrenghtText;
     private TextView mStrengthTitle;
-    private Button mRetakeButton;
+    private Button mToMenuButton;
     private ImageView mResultLine1;
     private ImageView mResultLine2;
     private ImageView mResultLine3;
@@ -44,6 +50,7 @@ public class ResultPageFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         // code to initialize the three strengths
         mResult = Result.getInstance();
         mStrengths = QuestionFragment.newInstance().mStrengths;
@@ -59,60 +66,51 @@ public class ResultPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_result, container, false);
 
+
+        // set the references
         mResultIcon1 = (ImageView) view.findViewById(R.id.imageview_result_one);
         mResultIcon2 = (ImageView) view.findViewById(R.id.imageview_result_two);
         mResultIcon3 = (ImageView) view.findViewById(R.id.imageview_result_three);
         mStrenghtText = (TextView) view.findViewById(R.id.textview_result_strentgh_text);
         mStrengthTitle = (TextView) view.findViewById(R.id.textview_result_strenght_title);
-        mRetakeButton = (Button) view.findViewById(R.id.button_result_retake);
+        mToMenuButton = (Button) view.findViewById(R.id.button_result_to_menu);
         //mResultLine1 = (ImageView) view.findViewById(R.id.imageview_result_line);
         //mResultLine2 = (ImageView) view.findViewById(R.id.imageview_result_line_two);
         //mResultLine3 = (ImageView) view.findViewById(R.id.imageview_result_line_three);
 
-        mStrenghtText.setText(mStrengths[mNr1Strength].getDescriptionID());
-        mStrengthTitle.setText(mStrengths[mNr1Strength].getTitleID());
 
+        // set images for the results
         mResultIcon1.setImageResource(mStrengths[mNr1Strength].getIconID());
         mResultIcon2.setImageResource(mStrengths[mNr2Strength].getIconID());
         mResultIcon3.setImageResource(mStrengths[mNr3Strength].getIconID());
-        mResultIcon1.setAlpha(1.0f);
-        mResultIcon2.setAlpha(0.4f);
-        mResultIcon3.setAlpha(0.4f);
 
+
+        // set listeners
         mResultIcon1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mStrengthTitle.setText(mStrengths[mNr1Strength].getTitleID());
-                mStrenghtText.setText(mStrengths[mNr1Strength].getDescriptionID());
-                mResultIcon1.setAlpha(1.0f);
-                mResultIcon2.setAlpha(0.4f);
-                mResultIcon3.setAlpha(0.4f);
+                mCurrentStrength = 0;
+                setPickedResult();
             }
         });
 
         mResultIcon2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mStrengthTitle.setText(mStrengths[mNr2Strength].getTitleID());
-                mStrenghtText.setText(mStrengths[mNr2Strength].getDescriptionID());
-                mResultIcon1.setAlpha(0.4f);
-                mResultIcon2.setAlpha(1.0f);
-                mResultIcon3.setAlpha(0.4f);
+                mCurrentStrength = 1;
+                setPickedResult();
             }
         });
 
         mResultIcon3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mStrengthTitle.setText(mStrengths[mNr3Strength].getTitleID());
-                mStrenghtText.setText(mStrengths[mNr3Strength].getDescriptionID());
-                mResultIcon1.setAlpha(0.4f);
-                mResultIcon2.setAlpha(0.4f);
-                mResultIcon3.setAlpha(1.0f);
+                mCurrentStrength = 2;
+                setPickedResult();
             }
         });
 
-        mRetakeButton.setOnClickListener(new View.OnClickListener() {
+        mToMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = StartPageActivity.newIntent(getActivity());
@@ -121,7 +119,53 @@ public class ResultPageFragment extends Fragment {
             }
         });
 
+
+        // set the current strenght, if there use the savedInstanceState
+        if (savedInstanceState != null) {
+            mCurrentStrength = savedInstanceState.getInt(KEY_CURRENT_STRENGTH);
+        } else {
+            mCurrentStrength = 0;
+        }
+        setPickedResult();
+
         return view;
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_STRENGTH, mCurrentStrength);
+    }
+
+
+    private void setPickedResult() {
+        switch (mCurrentStrength) {
+            case 0:
+                mStrengthTitle.setText(mStrengths[mNr1Strength].getTitleID());
+                mStrenghtText.setText(mStrengths[mNr1Strength].getDescriptionID());
+                mResultIcon1.setAlpha(1.0f);
+                mResultIcon2.setAlpha(0.4f);
+                mResultIcon3.setAlpha(0.4f);
+                return;
+            case 1:
+                mStrengthTitle.setText(mStrengths[mNr2Strength].getTitleID());
+                mStrenghtText.setText(mStrengths[mNr2Strength].getDescriptionID());
+                mResultIcon1.setAlpha(0.4f);
+                mResultIcon2.setAlpha(1.0f);
+                mResultIcon3.setAlpha(0.4f);
+                return;
+            case 2:
+                mStrengthTitle.setText(mStrengths[mNr3Strength].getTitleID());
+                mStrenghtText.setText(mStrengths[mNr3Strength].getDescriptionID());
+                mResultIcon1.setAlpha(0.4f);
+                mResultIcon2.setAlpha(0.4f);
+                mResultIcon3.setAlpha(1.0f);
+                return;
+            default:
+                Log.e(TAG, "Picked result " + mCurrentStrength + " does not exist");
+        }
     }
 
 }
