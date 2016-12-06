38b6dce8fb182269d4612372536908d26d0e29c1
Jarle <jarle.matland@gmail.com>
2016-12-06 17:29:53 +0100

    - updated the file so that when Start is pressed you will get a dialog that will allow you to take the test for yourself or (if logged in) for others

--- app/src/main/java/com/cloud9/android/happycode/StartPageFragment.java
+++ app/src/main/java/com/cloud9/android/happycode/StartPageFragment.java
@@ -1,33 +1,21 @@
 package com.cloud9.android.happycode;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.Date;
 
 /**
  * Created by paulvancappelle on 22-11-16.
@@ -35,11 +23,15 @@ import java.util.Date;
 
 public class StartPageFragment extends Fragment {
 
+    private static final String DIALOG_INVITE = "dialog_invite";
+    private static final String USER_ID_FROM_TESTER = "user_id_from_tester";
+
     Button mStartButton;
     Button mAboutButton;
     Button mTestHistoryButton;
     Button mAllCodes;
     Button mLogInButton;
+    Button mInviteButton;
 
     private TestResultList mTestResultList;
 
@@ -79,14 +71,18 @@ public class StartPageFragment extends Fragment {
         mAllCodes = (Button) view.findViewById(R.id.button_all_codes);
         mTestHistoryButton = (Button) view.findViewById(R.id.button_test_history);
         mLogInButton = (Button) view.findViewById(R.id.buttonLogInStartPage);
+        mInviteButton = (Button) view.findViewById(R.id.buttonOtherTester);
 
         // set listeners
         mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mStartButton.setBackgroundResource(R.drawable.button_pressed);
-                Intent i = QuestionActivity.newIntent(getActivity());
-                startActivity(i);
+
+                FragmentManager fragmentManager = getFragmentManager();
+                QuestionDialogFragment questionDialogFragment = new QuestionDialogFragment();
+                questionDialogFragment.show(fragmentManager, USER_ID_FROM_TESTER);
+
             }
         });
 
@@ -127,6 +123,20 @@ public class StartPageFragment extends Fragment {
                 } else {
                     Intent i = LogInActivity.newIntent(getActivity());
                     startActivity(i);
+                }
+            }
+        });
+
+        mInviteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (mUser == null) {
+                    Toast.makeText(getActivity(), "You must be logged in to do this", Toast.LENGTH_SHORT).show();
+                } else {
+                    FragmentManager fragmentManager = getFragmentManager();
+                    InviteTesterDialogFragment inviteTesterDialogFragment = new InviteTesterDialogFragment();
+                    inviteTesterDialogFragment.show(fragmentManager, DIALOG_INVITE);
                 }
             }
         });
