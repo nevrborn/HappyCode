009aecb1c7a917f82e754c97b6181fef7feef902
Jarle <jarle.matland@gmail.com>
2016-12-06 17:32:31 +0100

    - added that the userID is being shipped to the fragment

--- app/src/main/java/com/cloud9/android/happycode/QuestionActivity.java
+++ app/src/main/java/com/cloud9/android/happycode/QuestionActivity.java
@@ -10,6 +10,10 @@ import android.support.v4.app.Fragment;
 
 public class QuestionActivity extends SingleFragmentActivity {
 
+    private static final String USER_ID_FROM_TESTER = "user_id_from_tester";
+
+    String tester_id;
+
     /*
     * create Intent to start this activity
     */
@@ -21,7 +25,8 @@ public class QuestionActivity extends SingleFragmentActivity {
     /* Method to create fragment from Activity */
     @Override
     protected Fragment createFragment() {
-        return QuestionFragment.newInstance();
+        tester_id = (String) getIntent().getExtras().get(USER_ID_FROM_TESTER);
+        return QuestionFragment.newInstance(tester_id);
     }
 
 }
