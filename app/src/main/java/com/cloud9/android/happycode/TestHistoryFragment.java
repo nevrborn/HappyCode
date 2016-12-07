bf7d0fee5f0527d23d39173549864f9019d593c9
Jarle <jarle.matland@gmail.com>
2016-12-07 17:17:00 +0100

    - added static row on top of the recycler view to display the top 3 strengths overall

--- app/src/main/java/com/cloud9/android/happycode/TestHistoryFragment.java
+++ app/src/main/java/com/cloud9/android/happycode/TestHistoryFragment.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -21,8 +22,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by paulvancappelle on 24-11-16.
@@ -33,6 +37,17 @@ public class TestHistoryFragment extends Fragment {
     private RecyclerView mTestRecyclerView;
     private TestResultsAdapter mTestResultsAdapter;
     private TestResultList mTestResultList;
+    private StrengthList mStrengths = StrengthList.get(getContext());
+    public static Map<String, Integer> arrayOfTotalResults = new HashMap<>();
+    public static ArrayList<String> mTotalTop3StrengthKeys = new ArrayList<String>();
+
+    private ImageView mTotalTop1StrengthImage;
+    private ImageView mTotalTop2StrengthImage;
+    private ImageView mTotalTop3StrengthImage;
+    private TextView mTotalTop1StrengthText;
+    private TextView mTotalTop2StrengthText;
+    private TextView mTotalTop3StrengthText;
+    private RelativeLayout mTop3CodeLayout;
 
     private DatabaseReference mUserRef;
 
@@ -68,7 +83,22 @@ public class TestHistoryFragment extends Fragment {
         mTestRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerview_test_history);
         mTestRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        mTotalTop1StrengthImage = (ImageView) view.findViewById(R.id.list_item_icon_1_top);
+        mTotalTop2StrengthImage = (ImageView) view.findViewById(R.id.list_item_icon_2_top);
+        mTotalTop3StrengthImage = (ImageView) view.findViewById(R.id.list_item_icon_3_top);
+        mTotalTop1StrengthText = (TextView) view.findViewById(R.id.textview_code_1_top);
+        mTotalTop2StrengthText = (TextView) view.findViewById(R.id.textview_code_2_top);
+        mTotalTop3StrengthText = (TextView) view.findViewById(R.id.textview_code_3_top);
+        mTop3CodeLayout = (RelativeLayout) view.findViewById(R.id.totalTopCodes_relativelayout);
+
         updateUI();
+
+        if (mTestResultList.getSize() > 1) {
+            setTotalTop3Strengths();
+            mTop3CodeLayout.setVisibility(View.VISIBLE);
+        } else {
+            mTop3CodeLayout.setVisibility(View.GONE);
+        }
 
         return view;
     }
@@ -84,11 +114,20 @@ public class TestHistoryFragment extends Fragment {
 
         mTestResultsAdapter = new TestResultsAdapter(TestResultList.getTestResultList());
         mTestRecyclerView.setAdapter(mTestResultsAdapter);
+
+
     }
 
     @Override
     public void onResume() {
         super.onResume();
+
+        if (mTestResultList.getSize() != 0) {
+            setTotalTop3Strengths();
+            mTop3CodeLayout.setVisibility(View.VISIBLE);
+        } else {
+            mTop3CodeLayout.setVisibility(View.GONE);
+        }
     }
 
     public void getDataFromFirebase() {
@@ -109,6 +148,82 @@ public class TestHistoryFragment extends Fragment {
 
             }
         });
+
+        setTotalTop3Strengths();
+
+    }
+
+    private void makeTotalResultArray(TestResultList testResultList) {
+
+        arrayOfTotalResults.clear();
+        int sizeOfArray = testResultList.getSize();
+        int i = 0;
+        int totalPercentage = 0;
+        ArrayList<String> listOfAllCodes = mStrengths.getAllKeys();
+
+
+        while (i < listOfAllCodes.size()) {
+            String key = listOfAllCodes.get(i);
+
+            int j = 0;
+            totalPercentage = 0;
+
+            while (j < sizeOfArray) {
+                totalPercentage = totalPercentage + testResultList.getTestResultFromIndex(j).getResultArray().get(key);
+                j += 1;
+            }
+
+            arrayOfTotalResults.put(key, totalPercentage);
+
+            i += 1;
+        }
+
+        findTop3Strengths(arrayOfTotalResults);
+
+    }
+
+    public static void findTop3Strengths(Map<String, Integer> resultArray) {
+        Map<String, Integer> tempArray = new HashMap<>(resultArray);
+
+        mTotalTop3StrengthKeys.clear();
+
+        String keyOfMaxValue = "";
+
+        int j = 0;
+
+        while (j < 3) {
+
+            int maxValue = Collections.max(tempArray.values());
+
+            for (Map.Entry<String, Integer> entry : tempArray.entrySet()) {
+                if (entry.getValue() == maxValue) {
+                    keyOfMaxValue = entry.getKey();
+                }
+            }
+
+            mTotalTop3StrengthKeys.add(keyOfMaxValue);
+            tempArray.remove(keyOfMaxValue);
+            j += 1;
+        }
+
+        tempArray.clear();
+    }
+
+    public void setTotalTop3Strengths() {
+
+        makeTotalResultArray(mTestResultList);
+
+        Strength mTop1Strength = mStrengths.getStrengthFromKey(mTotalTop3StrengthKeys.get(0));
+        Strength mTop2Strength = mStrengths.getStrengthFromKey(mTotalTop3StrengthKeys.get(1));
+        Strength mTop3Strength = mStrengths.getStrengthFromKey(mTotalTop3StrengthKeys.get(2));
+
+        mTotalTop1StrengthImage.setImageResource(mTop1Strength.getIconID());
+        mTotalTop2StrengthImage.setImageResource(mTop2Strength.getIconID());
+        mTotalTop3StrengthImage.setImageResource(mTop3Strength.getIconID());
+
+        mTotalTop1StrengthText.setText(mTop1Strength.getTitleID());
+        mTotalTop2StrengthText.setText(mTop2Strength.getTitleID());
+        mTotalTop3StrengthText.setText(mTop3Strength.getTitleID());
     }
 
     /*
