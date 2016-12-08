49e9db9e0cc64fd73f7b9441327f1d7c1721e788
Jarle <jarle.matland@gmail.com>
2016-12-08 10:54:13 +0100

    -added links to NJR and Cloud9 websites

--- app/src/main/java/com/cloud9/android/happycode/AboutPageFragment.java
+++ app/src/main/java/com/cloud9/android/happycode/AboutPageFragment.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by paulvancappelle on 24-11-16.
@@ -18,6 +20,12 @@ public class AboutPageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView njr_link = (TextView) view.findViewById(R.id.textview_about_link_to_njr_website);
+        TextView cloud9_link = (TextView) view.findViewById(R.id.textview_about_link_to_cloud9_website);
+
+        njr_link.setMovementMethod(LinkMovementMethod.getInstance());
+        cloud9_link.setMovementMethod(LinkMovementMethod.getInstance());
 
         return view;
     }
